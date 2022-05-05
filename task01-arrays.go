225eeb8e31c5a4a5d969f6690ccc36c59861121e
Bekbolat Kuralbayev <bkb@live.ru>
2022-05-06 02:25:00 +0600

    completed solution

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,9 @@
 package homework
 
+// returns an average value of array (sum / N)
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	for _, value := range input {
+		result += value
+	}
+	return result / 15
 }
--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
+// returns the copy of the original slice in reverse order. The type of elements is int64.
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+	for i := len(input) - 1; i >= 0; i-- {
+		result = append(result, input[i])
+	}
 	return
 }
